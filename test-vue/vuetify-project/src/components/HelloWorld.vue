51ec7a5105ce1fab8f2c390e478c792fc5eb6113
Masa <katsukatsu.masa.0522@gmail.com>
2021-04-05 20:05:56 +0900

    追加ボタンの実装

--- test-vue/vuetify-project/src/components/HelloWorld.vue
+++ test-vue/vuetify-project/src/components/HelloWorld.vue
@@ -4,6 +4,7 @@
       <v-toolbar-title>ToDo List</v-toolbar-title>
     </v-app-bar>
     <v-text-field placeholder="ToDoの入力"></v-text-field>
+    <v-btn color="primary" dark >追加</v-btn>
     <v-footer color="info" dark app>Vuetify</v-footer>
   </v-app>
 </template>
