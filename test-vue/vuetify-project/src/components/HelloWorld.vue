519c0d543cbbcee8f3bb5686c8fbdbc62bf7b24a
Masa <katsukatsu.masa.0522@gmail.com>
2021-04-05 19:47:43 +0900

    テキスト入力欄の追加

--- test-vue/vuetify-project/src/components/HelloWorld.vue
+++ test-vue/vuetify-project/src/components/HelloWorld.vue
@@ -3,6 +3,7 @@
     <v-app-bar color="info" dark app>
       <v-toolbar-title>ToDo List</v-toolbar-title>
     </v-app-bar>
+    <v-text-field placeholder="ToDoの入力"></v-text-field>
     <v-footer color="info" dark app>Vuetify</v-footer>
   </v-app>
 </template>
